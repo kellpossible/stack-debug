173d466c3455938ee27fe609ea2643dc0cea97ed
Luke Frisken <l.frisken@gmail.com>
2025-07-23 16:13:23 +1000

    Initial commit

---
+++ examples/example/src/main.rs
@@ -0,0 +1,23 @@
+#[stack_debug::instrument]
+fn function_with_large_stack_frame() {
+    // Allocate 4KB on the stack to make the frame size obvious.
+    let my_local_data = [0u8; 4096];
+    println!("{my_local_data:?}");
+    nested();
+}
+
+#[stack_debug::instrument]
+fn nested() {
+    let my_local_data = [0u8; 128];
+    println!("{my_local_data:?}");
+}
+
+#[stack_debug::instrument]
+fn function_with_small_stack_frame() {
+}
+
+fn main() {
+    tracing_subscriber::fmt().init();
+    function_with_small_stack_frame();
+    function_with_large_stack_frame();
+}
---
+++ src/lib.rs
@@ -0,0 +1,98 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, ItemFn};
+
+#[proc_macro_attribute]
+pub fn instrument(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as ItemFn);
+
+    // Ensure no attributes are passed to `instrument` for now.
+    // If attributes were needed, they would be parsed from `attr`.
+    if !attr.is_empty() {
+        return syn::Error::new_spanned(
+            proc_macro2::TokenStream::from(attr),
+            "#[stack_debug::instrument] does not take any arguments",
+        )
+        .to_compile_error()
+        .into();
+    }
+
+    let block = input.block;
+    let sig = input.sig;
+    let vis = input.vis;
+    let attrs = input.attrs;
+
+    #[cfg(feature = "tracing")]
+    let expanded = quote! {
+        #(#attrs)*
+        #[inline(never)]
+        #[tracing::instrument]
+        #vis #sig {
+            let rbp: usize;
+            let rsp: usize;
+
+            unsafe {
+                // These instructions are for x86_64.
+                // Other architectures like AArch64 may have different
+                // conventions or registers (e.g., `fp`).
+                #[cfg(target_arch = "x86_64")]
+                std::arch::asm!(
+                    "mov {}, rbp", // Get the base pointer
+                    "mov {}, rsp", // Get the stack pointer
+                    out(reg) rbp,
+                    out(reg) rsp,
+                );
+                // Add cfgs for other architectures if needed.
+            }
+
+            // rbp should be greater than rsp. If it's not, frame pointers
+            // were likely omitted, and the result is meaningless.
+            #[cfg(debug_assertions)]
+            let frame_size = rbp - rsp - 1520; // subtract overhead of instrumentation
+            #[cfg(not(debug_assertions))]
+            let frame_size = rbp - rsp - 224; // subtract overhead of instrumentation
+            tracing::info!("stack frame size: {frame_size}");
+
+            #block
+        }
+    };
+
+    #[cfg(not(feature = "tracing"))]
+    let expanded = {
+        let function_name = sig.ident.to_string();
+        quote! {
+            #(#attrs)*
+            #[inline(never)]
+            #vis #sig {
+                let rbp: usize;
+                let rsp: usize;
+
+                unsafe {
+                    // These instructions are for x86_64.
+                    // Other architectures like AArch64 may have different
+                    // conventions or registers (e.g., `fp`).
+                    #[cfg(target_arch = "x86_64")]
+                    std::arch::asm!(
+                        "mov {}, rbp", // Get the base pointer
+                        "mov {}, rsp", // Get the stack pointer
+                        out(reg) rbp,
+                        out(reg) rsp,
+                    );
+                    // Add cfgs for other architectures if needed.
+                }
+
+                // rbp should be greater than rsp. If it's not, frame pointers
+                // were likely omitted, and the result is meaningless.
+                #[cfg(debug_assertions)]
+                let frame_size = rbp - rsp - 128; // subtract overhead of instrumentation
+                #[cfg(not(debug_assertions))]
+                let frame_size = rbp - rsp - 80; // subtract overhead of instrumentation
+                println!("{}::{}(): stack frame size: {frame_size}", module_path!(), #function_name);
+
+                #block
+            }
+        }
+    };
+
+    expanded.into()
+}
